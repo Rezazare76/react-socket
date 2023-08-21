df496874028f1efbad857aae57423f25d978e825
reza.zare <rezazare2088@gmail.com>
2023-08-21 10:38:09 +0430

    Adding dist

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,7 @@
 /// <reference types="vite/client" />
+import { defineConfig } from "vite";
+import React from "@vitejs/plugin-react";
+export default defineConfig({
+  base: "/react-socket/",
+  Plugin: [React],
+});
